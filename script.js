431842782d6da83eb669cce2352b5473d5715264
Sameh El Zankalony <zankalony@gmail.com>
2024-01-07 02:31:57 +0200

    updating the append child functionality

--- script.js
+++ script.js
@@ -5,17 +5,19 @@ document.addEventListener('DOMContentLoaded', function () {
     e.preventDefault();
     var today = new Date();
     var specificDate = new Date(document.getElementById('date').value);
-    // console.log(specificDate);
     var timestampToday = today.valueOf();
     var timestampSpecificDate = specificDate.valueOf();
 
     var millisecondsPerDay = 1000 * 60 * 60 * 24;
     var daysDiff =
       (timestampToday - timestampSpecificDate) / millisecondsPerDay;
-    // console.log('Difference in days: ' + Math.abs(Math.round(daysDiff))); // Math.abs ensures the difference is always positive
 
-    var result = document.createElement('div');
+    var result = document.getElementById('result');
+    if (!result) {
+      result = document.createElement('div');
+      result.setAttribute('id', 'result');
+      document.body.appendChild(result);
+    }
     result.innerHTML = 'Difference in days: ' + Math.abs(Math.round(daysDiff));
-    document.body.appendChild(result);
   });
 });
